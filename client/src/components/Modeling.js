bd69fb7201edf232aef3b8d8e3ae565d054eddcf
Tuyen Pham <tuh39709@temple.edu>
2020-10-08 21:34:06 -0400

    Upload Image button in frontend

--- client/src/components/Modeling.js
+++ client/src/components/Modeling.js
@@ -4,26 +4,17 @@ export default class Modeling extends Component {
   
   render(){
     return (
-      <div>
-      <label htmlFor="upload-button">
-        
-          <>
-            <span className="fa-stack fa-2x mt-3 mb-2">
-              <i className="fas fa-circle fa-stack-2x" />
-              <i className="fas fa-store fa-stack-1x fa-inverse" />
-            </span>
-            <h5 className="text-center">Upload your photo</h5>
-          </>
-      </label>
-      <input
-        type="file"
-        id="upload-button"
-        style={{ display: "none" }}
-        
-      />
-      <br />
-      <button >Upload</button>
-    </div>
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+      
+      </div>
     )
   }
 }

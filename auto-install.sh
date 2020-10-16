96f4e66885780c4b8bb0a1adafe9d3053046fbaf
Khai Nguyen <tuh23333@templ.edu>
2020-10-16 14:44:50 +0000

    Auto install sript added

---
+++ auto-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Sript to insstall all dependencies
+
+killall node
+sudo rm -rf node_modules
+npm cache clean --force
+
+
+
+npm install 
+npm i react-router-dom
+npm install --save react-router
+npm install axios
+npm install typescript
+npm install --save react-toastify

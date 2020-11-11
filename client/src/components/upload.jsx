f57851a005090e973c4dfac2ab39b45fce8ed99d
lizaibeme <tug08848@temple.edu>
2020-11-11 08:24:15 -0500

    fixed layout of the uplouad page

--- client/src/components/upload.jsx
+++ client/src/components/upload.jsx
@@ -1,4 +1,7 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
+import { Row, Col } from 'react-bootstrap'
+import Rend from './rend'
+
 
 const Upload = () => {
   let [file, setFile] = useState(null);
@@ -20,76 +23,76 @@ const Upload = () => {
     /**Capture filename */
     setFilename(file.name);
     console.log(file);
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       setFile(e.target.result);
     };
     reader.readAsDataURL(event.target.files[0]);
 
-  //   if (file != ".png" ) {
-  //     window.alert("File does not support. You must use .png or .jpg ");
-  //     return false;
-  //  }
-  //  if (file.size > 10e6) {
-  //    window.alert("Please upload a file smaller than 10 MB");
-  //    return false;
-  //  }
-   
+    //   if (file != ".png" ) {
+    //     window.alert("File does not support. You must use .png or .jpg ");
+    //     return false;
+    //  }
+    //  if (file.size > 10e6) {
+    //    window.alert("Please upload a file smaller than 10 MB");
+    //    return false;
+    //  }
+
   };
 
-  const handleUpload = event =>{
+  const handleUpload = event => {
     event.preventDefault()
     var formData = new FormData();
-      /**
-     * Append 2 keys to the request body: the file name
-     * and the file blob itself.
-     */
+    /**
+   * Append 2 keys to the request body: the file name
+   * and the file blob itself.
+   */
     formData.append('fileblob', file);
     formData.append('filename', filename);
     console.log(filename);
-      fetch('http://35.196.125.66:9001/uploads', {
-        method:'POST',
-         body: formData
-      }) 
+    fetch('http://35.196.125.66:9001/uploads', {
+      method: 'POST',
+      body: formData
+    })
       .then(response => response.json())
       .then(data => console.log(data));
   }
-  
-    return (
-      <div id="upload">
-        <div className="container">
-          <div className="row">
-            <div className="col-xs-12 col-md-8"> 
-              <div className="upload-text">
-                <h3>Upload a 2D image to get a 3D model</h3>        
-                
-                <input className="btn btn-secondary" 
-                  id="fileInput" 
-                  type="file" 
-                  accept="image/*"
-                  // inputProps={{ accept: 'image/*' }}
-                  // fileFilter={filterBySize}
-                  onChange={fileChangedHandler} 
-                />
-                
 
-                <button className="btn btn-primary" style={{float:"left", margin: "0px"}}
-                          id="renderButton" onClick={handleUpload}>
-                  Upload
+  return (
+    <div id="upload">
+      <div className="container">
+        <Row>
+          <Col md={6}>
+            <div className="upload-text">
+              <h3>Upload a 2D image to get a 3D model</h3>
+
+              <input className="btn btn-secondary"
+                id="fileInput"
+                type="file"
+                accept="image/*"
+                // inputProps={{ accept: 'image/*' }}
+                // fileFilter={filterBySize}
+                onChange={fileChangedHandler}
+              />
+              <button className="btn btn-primary" style={{ float: "left", margin: "0px" }}
+                id="renderButton" onClick={handleUpload}>
+                Upload
                 </button>
-             
-                <button className="btn btn-primary" style={{float:"left", marginLeft: "10px", marginBottom: "10px"}} 
-                        id="renderButton" >
-                  Render 3D Model
+              <button className="btn btn-primary" style={{ float: "left", marginLeft: "10px", marginBottom: "10px" }}
+                id="renderButton" >
+                Render 3D Model
                 </button>
-              
-              </div>
             </div>
-          </div>
-          <img src={file} alt={""} width="400" height="400" text-align="left" style={{display:'block'}} />
-        </div>
+            <img src={file} alt={""} width="400" height="400" text-align="left" style={{ display: 'block' }} />
+          </Col>
+          <Col md={6}>
+            <Rend />
+          </Col>
+        </Row>
       </div>
-    )
-  }
+     
+    </div>
+  )
+}
 
 
 export default Upload;

c8ebd72714429cbe49527cc9d5acc3322b048e27
KhaiTTNguyen <tuh23333@temple.edu>
2020-12-04 19:49:19 -0500

    exec

--- server/helpers/spawnExec.js
+++ server/helpers/spawnExec.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 const spawnExec = () => new Promise((resolve, reject) => {
     /**
      */
-    const ls = spawn('sh network-call.sh', [shell=/bin/sh]); /// this is our command
+    const ls = spawn('sh network-call.sh', [shell=true]); /// this is our command
     // python2 demo.py --image Data/examples/plane.png
 
     ls.stdout.on("data", data => {

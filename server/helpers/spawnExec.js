abe1ed26239c91b1010632a89388182bc94d1038
KhaiTTNguyen <tuh23333@temple.edu>
2020-12-06 21:16:36 -0500

    Testing spanwExec

--- server/helpers/spawnExec.js
+++ server/helpers/spawnExec.js
@@ -1,9 +1,9 @@
 const { spawn } = require('child_process');
 
-const spawnExec = (imageFilename) => new Promise((resolve, reject) => {
+const spawnExec = (reqBody) => new Promise((resolve, reject) => {
     /**
      */
-    // imageFilename = "plane.png";
+    var imageFilename = reqBody.file;
     const ls = spawn('sh network-call.sh',[imageFilename],{ shell: true }); /// this is our command
     // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
 
